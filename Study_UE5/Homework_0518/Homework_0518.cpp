5a49af26f52d35523f6e2e95f52dfa4cbe79c506
checci7 <checci78@gmail.com>
2023-05-19 10:05:22 +0900

    aa

---
+++ Study_UE5/Homework_0518/Homework_0518.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+int StringCount(char* _Arr)
+{
+    int ChCount = 0;
+
+    while (_Arr[ChCount])
+    {
+        ++ChCount;
+    }
+    return ChCount;
+}
+
+void ChangeCh(char* _Arr, char _PrevCh, char _NextCh)
+{
+    // 1. 들어온 글자를 전부 '1'로 채워봐라.
+    //  1-1. 함수 내부에서 하지 말고
+    // for while도 생각하지 말고
+
+
+    return;
+}
+
+int main()
+{
+    {
+        char Arr[100] = "2222";
+
+        int Result = StringCount(Arr);
+
+        std::cout << Result << std::endl;
+
+    }
+
+    {
+        char Arr[100] = "aaa bbb ccc";
+
+        Arr[0] = '1';
+
+        // Arr[100] = "1111111111111";
+
+        // 3이 나와야 합니다.
+        // "aaa bbb ccc"; => "aaa bbb ddd";
+        ChangeCh(Arr, 'c', 'd');
+    }
+}
