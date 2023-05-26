6e032f3fc1ed01e82049f7c2e844186e38fc1750
checci7 <checci78@gmail.com>
2023-05-26 09:00:00 +0900

    aa

---
+++ AR48__/AR48__/AR48__.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    void kfc(int arr[], int ind, int len);
+
+    int arr[5] = { 5,7,1,2,3 };
+
+    kfc(arr, 0, 5);
+
+    return 0;
+}
+
+void kfc(int arr[], int ind, int len)
+{
+    if (ind == len)
+        return;
+
+    std::cout << arr[ind];
+    kfc(arr, ind + 1, len);
+    std::cout << arr[ind];
+}
